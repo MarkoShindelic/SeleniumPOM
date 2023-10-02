5c30a92d0763dc0827a0b7101dc2fee5b23a1759
Marko <marko.shindelic88@gmail.com>
2023-10-02 19:59:21 +0200

    Adding negative login tests

--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -1,6 +1,7 @@
 package Tests;
 
 import Base.BaseTest;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -22,7 +23,79 @@ public class LoginTest extends BaseTest {
         loginPage.clickOnSubmitButton();
         Assert.assertTrue(profilePage.getMessage().isDisplayed());
         Assert.assertTrue(profilePage.getLogOut().isDisplayed());
-
     }
+
+    @Test
+    public void userCannotLogInWithEmptyUsername() {
+
+        homePage.clickOnPracticeButton();
+        practicePage.clickOnTestLoginPageButton();
+        loginPage.inputUsername(" ");
+        loginPage.inputPassword("Password123");
+        loginPage.clickOnSubmitButton();
+
+        wait.until(ExpectedConditions.visibilityOf(loginPage.getError()));
+        Assert.assertTrue(loginPage.getError().isDisplayed());
+    }
+
+    @Test
+    public void userCannotLogInWithInvalidUsername(){
+        homePage.clickOnPracticeButton();
+        practicePage.clickOnTestLoginPageButton();
+        loginPage.inputUsername("SomeUsername");
+        loginPage.inputPassword("Password123");
+        loginPage.clickOnSubmitButton();
+
+        wait.until(ExpectedConditions.visibilityOf(loginPage.getError()));
+        Assert.assertTrue(loginPage.getError().isDisplayed());
+    }
+    @Test
+    public void userCannotLogInWithEmptyPassword(){
+        homePage.clickOnPracticeButton();
+        practicePage.clickOnTestLoginPageButton();
+        loginPage.inputUsername("student");
+        loginPage.inputPassword(" ");
+        loginPage.clickOnSubmitButton();
+
+        wait.until(ExpectedConditions.visibilityOf(loginPage.getError()));
+        Assert.assertTrue(loginPage.getError().isDisplayed());
+    }
+
+    @Test
+    public void userCannotLogInWithInvalidPassword(){
+        homePage.clickOnPracticeButton();
+        practicePage.clickOnTestLoginPageButton();
+        loginPage.inputUsername("student");
+        loginPage.inputPassword("somePassword");
+        loginPage.clickOnSubmitButton();
+
+        wait.until(ExpectedConditions.visibilityOf(loginPage.getError()));
+        Assert.assertTrue(loginPage.getError().isDisplayed());
+    }
+
+    @Test
+    public void userCannotLogInWithInvalidCredentials(){
+        homePage.clickOnPracticeButton();
+        practicePage.clickOnTestLoginPageButton();
+        loginPage.inputUsername("someUser");
+        loginPage.inputPassword("somePassword");
+        loginPage.clickOnSubmitButton();
+
+        wait.until(ExpectedConditions.visibilityOf(loginPage.getError()));
+        Assert.assertTrue(loginPage.getError().isDisplayed());
+    }
+
+    @Test
+    public void userCannotLogInWithEmptyCredentials(){
+        homePage.clickOnPracticeButton();
+        practicePage.clickOnTestLoginPageButton();
+        loginPage.inputUsername(" ");
+        loginPage.inputPassword(" ");
+        loginPage.clickOnSubmitButton();
+
+        wait.until(ExpectedConditions.visibilityOf(loginPage.getError()));
+        Assert.assertTrue(loginPage.getError().isDisplayed());
+    }
+
 
 }
